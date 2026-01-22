3264af08d3a3b9c995323c056cb7fb562d2c55f8
EmmLSP <aexclusivasp@gmail..com>
2026-01-22 10:15:34 -0300

    curso java - aula 13 - exercicio 11

--- src/com/loiane/javabasico/aula13/labs/Exer11.java
+++ src/com/loiane/javabasico/aula13/labs/Exer11.java
@@ -1,11 +1,13 @@
 package com.loiane.javabasico.aula13.labs;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exer11 {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Entre com um numero inteiro:");
