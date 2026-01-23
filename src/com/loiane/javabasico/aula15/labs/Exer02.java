74595efa5b7505300a26fc483c84ccb5274f7b91
EmmLSP <aexclusivasp@gmail..com>
2026-01-23 09:50:25 -0300

    curso java - aula 14-15 - exercicio 02

---
+++ src/com/loiane/javabasico/aula15/labs/Exer02.java
@@ -0,0 +1,21 @@
+package com.loiane.javabasico.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com um numero:");
+		int numero = scan.nextInt();
+		
+		if (numero >= 0) {
+			System.out.println("O numero " + numero + " é POSITIVO");
+		} else {
+			System.out.println("O numero " + numero + " é NEGATIVO");
+		}
+	}
+
+}
