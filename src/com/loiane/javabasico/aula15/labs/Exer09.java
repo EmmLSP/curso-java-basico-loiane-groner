3c2048fbbac2d08712c8c3d40a558bec7c127863
EmmLSP <aexclusivasp@gmail..com>
2026-01-23 11:40:12 -0300

    curso java - aula 14-15 - exercicio 09

---
+++ src/com/loiane/javabasico/aula15/labs/Exer09.java
@@ -0,0 +1,57 @@
+package com.loiane.javabasico.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer09 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o primeiro numero:");
+		int num1 = scan.nextInt();
+		System.out.println("Entre com o segundo numero:");
+		int num2 = scan.nextInt();
+		System.out.println("Entre com o terceiro numero:");
+		int num3 = scan.nextInt();
+		
+		System.out.println("Numeros em ordem decrescente:");
+		
+		if (num1 >= num2 && num1 >= num3) {
+			if (num2 <= num3) {
+				System.out.println(num1 + " | " + num3 + " | " + num2);
+			} else {
+				System.out.println(num1 + " | " + num2 + " | " + num3);
+			}
+		} else if (num2 >= num1 && num2 >= num3) {
+			if (num1 <= num3) {
+				System.out.println(num2 + " | " + num3 + " | " + num1);
+			} else {
+				System.out.println(num2 + " | " + num1 + " | " + num3);
+			}
+		}  else if (num3 >= num1 && num3 >= num2) {
+			if (num1 <= num2) {
+				System.out.println(num3 + " | " + num2 + " | " + num1);
+			} else {
+				System.out.println(num3 + " | " + num1 + " | " + num2);
+			}
+		}
+		
+		System.out.println("Numeros em ordem decrescente:");
+		
+		if (num1 <= num2 && num1 <= num3 && num2 <= num3) {
+			System.out.println(num3 + " | " + num2 + " | " + num1);
+		} else if (num1 <= num2 && num1 <= num3 && num3 <= num2) {
+			System.out.println(num2 + " | " + num3 + " | " + num1);
+		} else if (num2 <= num1 && num2 <= num3 && num1 <= num3) {
+			System.out.println(num3 + " | " + num1 + " | " + num2);
+		} else if (num2 <= num1 && num2 <= num3 && num3 <= num1) {
+			System.out.println(num1 + " | " + num3 + " | " + num2);
+		} else if (num3 <= num1 && num3 <= num2 && num1 <= num2) {
+			System.out.println(num2 + " | " + num1 + " | " + num3);
+		} else if (num3 <= num1 && num3 <= num2 && num2 <= num1) {
+			System.out.println(num1 + " | " + num2 + " | " + num3);
+		}
+	}
+
+}
