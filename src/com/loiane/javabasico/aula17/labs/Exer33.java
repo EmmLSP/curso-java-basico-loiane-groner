3a12e0c4af0ecece81812d9e6545311021c03535
EmmLSP <aexclusivasp@gmail..com>
2026-01-29 10:32:15 -0300

    curso java - aula 16-17 - exercicio 33

---
+++ src/com/loiane/javabasico/aula17/labs/Exer33.java
@@ -0,0 +1,24 @@
+package com.loiane.javabasico.aula17.labs;
+
+import java.util.Scanner;
+
+public class Exer33 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com n termos:");
+		int n = scan.nextInt();
+		
+		double soma = 1;
+		System.out.print("S = ");
+		for (int i = 1, j = 1; i <= n; i++, j += 2) {
+			soma += i / j;
+			System.out.print(i + "/" + j + " + ");
+		}
+		System.out.println("... + n/m.");
+		System.out.println("Soma + " + soma);
+	}
+
+}
