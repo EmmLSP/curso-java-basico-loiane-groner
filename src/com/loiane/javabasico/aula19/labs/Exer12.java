ca5f7144c91f79719b461edbdd8248532cf11751
EmmLSP <aexclusivasp@gmail..com>
2026-01-29 16:34:01 -0300

    curso java - aula 19 - exercicio 12

---
+++ src/com/loiane/javabasico/aula19/labs/Exer12.java
@@ -0,0 +1,29 @@
+package com.loiane.javabasico.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer12 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		int soma = 0;
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor da posicao " + i);
+			vetorA[i] = scan.nextInt();
+			soma += vetorA[i];
+		}
+		
+		System.out.print("Vetor A = ");
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.print(vetorA[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.println("Soma: " + soma);
+	}
+
+}
