351a63e28fc8ac392f765f5b6f91d65547abdcd6
EmmLSP <aexclusivasp@gmail..com>
2026-01-31 11:36:57 -0300

    curso java - aula 19 - exercicio 32

---
+++ src/com/loiane/javabasico/aula19/labs/Exer32.java
@@ -0,0 +1,42 @@
+package com.loiane.javabasico.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer32 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor da posicao " + i);
+			vetorA[i] = scan.nextInt();
+			// vetorA[i] = (int) Math.round(Math.random() * 100);
+		}
+		
+		boolean primo;
+		for (int i = 0; i < vetorA.length; i++) {
+			primo = true;
+			if (vetorA[i] < 2) {
+				primo = false;
+			}
+			for (int j = 2; j < vetorA[i]; j++) {
+				if (vetorA[i] % j == 0) {
+					primo = false;
+					break;
+				}
+			}
+			String msg = "";
+			if (primo) {
+				msg = " - primo";
+			} else {
+				msg = " - nao é primo";
+			}
+			System.out.println(vetorA[i] + msg);
+		}
+		System.out.println();
+	}
+
+}
