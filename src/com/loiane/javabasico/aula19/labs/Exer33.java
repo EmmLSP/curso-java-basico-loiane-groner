39c38051bcb3e14fcbdeca12a740f5a48a91050c
EmmLSP <aexclusivasp@gmail..com>
2026-01-31 12:19:52 -0300

    curso java - aula 19 - exercicio 33

---
+++ src/com/loiane/javabasico/aula19/labs/Exer33.java
@@ -0,0 +1,29 @@
+package com.loiane.javabasico.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer33 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor da posicao " + i);
+			vetorA[i] = scan.nextInt();
+		}
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Analizando o numero " + vetorA[i] + ":");
+			for (int j = 2; j < vetorA[i]; j++) {
+				if (j % 2 == 0) {
+					System.out.println(j + " é par");
+				}
+			}
+			System.out.println();
+		}
+ 	}
+
+}
