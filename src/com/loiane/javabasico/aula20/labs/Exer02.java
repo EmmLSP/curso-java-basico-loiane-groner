823cf6dd889b13f22dbd128ff70011fe20b613a1
EmmLSP <aexclusivasp@gmail..com>
2026-02-01 11:41:41 -0300

    curso java - aula 20 - exercicio 02

---
+++ src/com/loiane/javabasico/aula20/labs/Exer02.java
@@ -0,0 +1,72 @@
+package com.loiane.javabasico.aula20.labs;
+
+import java.util.Random;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		int[][] numerosAleatorios = new int[10][10];
+		
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				numerosAleatorios[i][j] = random.nextInt(100);
+			}
+		}
+		
+		/*for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				if (numerosAleatorios[4][j] > maiorL5) {
+					maiorL5 = numerosAleatorios[4][j];
+				} else if (numerosAleatorios[4][j] < menorL5) {
+					menorL5 = numerosAleatorios[4][j];
+				}
+				
+				if (numerosAleatorios[i][6] > maiorC7) {
+					maiorC7 = numerosAleatorios[i][6];
+				} else if (numerosAleatorios[i][6] < menorC7) {
+					menorC7 = numerosAleatorios[i][6];
+				}
+			}
+		}*/
+		
+		int maiorL5 = Integer.MIN_VALUE;
+		int menorL5 = Integer.MAX_VALUE;
+		int linha5 = 5;
+		for (int i = 0; i < numerosAleatorios[linha5].length; i++) {
+			if (numerosAleatorios[5][i] > maiorL5) {
+				maiorL5 = numerosAleatorios[linha5][i];
+			}
+			if (numerosAleatorios[linha5][i] < menorL5) {
+				menorL5 = numerosAleatorios[linha5][i];
+			}
+		}
+		
+		int maiorC7 = Integer.MIN_VALUE;
+		int menorC7 = Integer.MAX_VALUE;
+		int col7 = 7;
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			if (numerosAleatorios[i][col7] > maiorC7) {
+				maiorC7 = numerosAleatorios[i][col7];
+			}
+			if (numerosAleatorios[i][col7] < menorC7) {
+				menorC7 = numerosAleatorios[i][col7];
+			}
+		}
+		
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			for (int j = 0; j < numerosAleatorios[i].length; j++) {
+				System.out.print(numerosAleatorios[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Maior valor da linha 5: " + maiorL5);
+		System.out.println("Menor valor da linha 5: " + menorL5);
+		System.out.println("Maior valor da coluna 7: " + maiorC7);
+		System.out.println("Menor valor da coluna 7: " + menorC7);
+	}
+
+}
