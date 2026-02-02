019b950e7b3380784e36cccd7c38115ffd28bf2f
EmmLSP <aexclusivasp@gmail..com>
2026-02-02 12:31:44 -0300

    curso java - poo - aula 24 - exercicio 01

---
+++ src/com/loiane/javabasico/aula24/labs/Exer01.java
@@ -0,0 +1,33 @@
+package com.loiane.javabasico.aula24.labs;
+
+public class Exer01 {
+	
+	public static void main(String[] args) {
+		
+		// new -> instanciar a lampada, objeto do tipo Lampada
+		Lampada lampada = new Lampada();
+		lampada.marca = "A60";
+		lampada.tensao = "Bivolt";
+		lampada.garantiaMeses = 36;
+		lampada.potencia = 7;
+		lampada.cor = "Amarela";
+		lampada.tipoLuz = "Amarela";
+		lampada.tipoAbajur = true;
+		
+		Lampada.tipos = new String[5];
+		lampada.tipos[0] = "Abajur";
+		lampada.tipos[1] = "lampeoes";
+		
+		System.out.println("Caracteristicas do objeto Lampada:");
+		System.out.println("Marca: " + lampada.marca);
+		System.out.println("Tensao: " + lampada.tensao);
+		System.out.println("Garantia em meses: " + lampada.garantiaMeses);
+		System.out.println("Potencia: " + lampada.potencia);
+		System.out.println("Cor: " + lampada.cor);
+		System.out.println("Tipo de luz: " + lampada.tipoLuz);
+		System.out.println("Lampada tipo Abajur: " + lampada.tipoAbajur);
+		System.out.println("Tipos de lampada: " + lampada.tipos[0]);
+		System.out.println("Tipos de lampada: " + lampada.tipos[1]);
+	}
+
+}
---
+++ src/com/loiane/javabasico/aula24/labs/Lampada.java
@@ -0,0 +1,13 @@
+package com.loiane.javabasico.aula24.labs;
+
+public class Lampada {
+	
+	String marca;
+	String tensao; // voltagem (110/220), Bivolt
+	int garantiaMeses;
+	double potencia; // potencia em watts (ex: 60W)
+	String cor; // cor da luz (branca, amarela, RGB)
+	String tipoLuz; // LED, fluoroscente, incandescete
+	static String[] tipos;
+	boolean tipoAbajur;
+}
