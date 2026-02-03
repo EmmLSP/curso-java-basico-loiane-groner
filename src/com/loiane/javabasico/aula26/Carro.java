a33df52c8937176ebab7b9bac729848c00792819
EmmLSP <aexclusivasp@gmail..com>
2026-02-03 12:21:13 -0300

    curso java - poo - aula 26

---
+++ src/com/loiane/javabasico/aula26/Carro.java
@@ -0,0 +1,19 @@
+package com.loiane.javabasico.aula26;
+
+public class Carro {
+
+		String marca;
+		String modelo;
+		int numPassageiros;
+		double capCombustivel;
+		double consumoCombustivel;
+		
+		void exibirAutonomia() {
+			System.out.println("A autonomia do carro é         : " + (capCombustivel * consumoCombustivel) + " km");
+		}
+
+		double obterAutonomia() {
+			System.out.println("Metodo obterAutonomia foi chamado.");
+			return capCombustivel * consumoCombustivel;
+		}
+}
---
+++ src/com/loiane/javabasico/aula26/TesteCarro.java
@@ -0,0 +1,28 @@
+package com.loiane.javabasico.aula26;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+		Carro van = new Carro();
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		System.out.println("Marca                          : " + van.marca);
+		System.out.println("Modelo                         : " + van.modelo);
+		System.out.println("Numero de passageiros          : " + van.numPassageiros);
+		System.out.println("Capacidade de combustivel      : " + van.capCombustivel);
+		System.out.println("Consumo de combustivel         : " + van.consumoCombustivel);
+		
+		van.exibirAutonomia();
+		double autonomia = van.obterAutonomia();
+		
+		System.out.println("A autonomia do carro é         : " + autonomia + " km");
+		System.out.println("A autonomia do carro é         : " + van.obterAutonomia() + " km");
+	}
+	
+
+}
