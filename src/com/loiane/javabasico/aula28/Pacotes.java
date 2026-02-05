4642b4f9531e46da9b0737b201e5e00f7d215d3a
EmmLSP <aexclusivasp@gmail..com>
2026-02-05 13:48:16 -0300

    curso java - aula 28

---
+++ src/com/loiane/javabasico/aula28/Pacotes.java
@@ -0,0 +1,20 @@
+package com.loiane.javabasico.aula28;
+
+import com.loiane.javabasico.aula20.labs.Exer04;
+import com.loiane.javabasico.aula20.labs.Exer05;
+import com.loiane.javabasico.aula20.labs.Exer06;
+import com.loiane.javabasico.aula27.Carro;
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+		
+		Carro carro;
+		com.loiane.javabasico.aula26.Carro carro2;
+		
+		Exer06 exer06;
+		Exer05 exer05;
+		Exer04 exer04;
+	}
+
+}
